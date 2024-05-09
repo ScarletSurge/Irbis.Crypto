2159e2fce966aca99474e39d1b046edae952b0e6
scarletsurge <scarletsurge.m@gmail.com>
2024-05-09 22:24:49 +0300

    added nuget settings, removed unused code

--- Demo/Irbis.Crypto.Demo.SHACAL2/Program.cs
+++ Demo/Irbis.Crypto.Demo.SHACAL2/Program.cs
@@ -1,15 +1,3 @@
-﻿using Irbis.Crypto.Domain;
+﻿// See https://aka.ms/new-console-template for more information
 
-var randomSource = new Random(782349);
-
-var keyBytes = new byte[16];
-randomSource.NextBytes(keyBytes);
-var ivBytes = new byte[16];
-randomSource.NextBytes(ivBytes);
-var ivBytesCopied = (byte[])ivBytes.Clone();
-var dataBytes = new byte[64];
-randomSource.NextBytes(dataBytes);
-
-var algorithm = new CipherSample(keyBytes);
-await CryptoTransformationContext.PerformCipherAsync(algorithm, 4, CipherMode.CounterMode, PaddingMode.ISO10126, CipherTransformationMode.Encryption, ivBytes, "in.jpg", "out.jpg").ConfigureAwait(false);
-await CryptoTransformationContext.PerformCipherAsync(algorithm, 4, CipherMode.CounterMode, PaddingMode.ISO10126, CipherTransformationMode.Decryption, ivBytesCopied, "out.jpg", "in1.jpg").ConfigureAwait(false);
+Console.WriteLine("Hello, World!");
